f306b04a7c45501a07441ba742d8d241e0239d93
Pratush <pratushbarua26@gmail.com>
2025-11-24 23:16:35 +0600

    fixed coverage area

--- src/Pages/coverage/CoverageArea.jsx
+++ src/Pages/coverage/CoverageArea.jsx
@@ -1,7 +1,7 @@
 import { MapContainer, Marker, Popup, TileLayer } from "react-leaflet";
 import 'leaflet/dist/leaflet.css'
 import { useForm } from "react-hook-form";
-import { useRef } from "react";
+import { useEffect, useRef, useState } from "react";
 import { useQuery } from "@tanstack/react-query";
 import axios from "axios";
 import Loader from "../../Shared/Loader";
@@ -10,20 +10,26 @@ import Error from "../../Shared/Error";
 
 export default function CoverageAreaPage() {
     const mapRef = useRef(null)
+    const [targetPosition, setTargetPosition] = useState({})
     const { register, handleSubmit, formState: { errors, isSubmitting } } = useForm()
-    
+
     const { data, isLoading, error: dataError } = useQuery({
         queryKey: ['reviews'],
         queryFn: () => axios(`${import.meta.env.VITE_SERVER}/ware-houses`).then(res => res.data),
         staleTime: 5 * 60 * 1000,
     })
 
+    useEffect(() => {
+        const viewZoom = 18
+        mapRef?.current?.flyTo([targetPosition.latitude, targetPosition.longitude], viewZoom)
+    }, [targetPosition])
+
+
     if (isLoading) return <Loader />;
     if (dataError) return <Error msg={dataError.message} />;
     const onSubmit = (typed) => {
-        const target = data.find(e => e.city.toLowerCase().includes(typed.search.toLowerCase()))
-        const viewZoom = 18
-        if (target) mapRef?.current?.flyTo([target.latitude, target.longitude], viewZoom)
+        const target = data.find(e => e.district.toLowerCase().includes(typed.search.toLowerCase()))
+        setTargetPosition({latitude: target.latitude, longitude: target.longitude})
     }
     return (
         <main className="my-8 w-full">
