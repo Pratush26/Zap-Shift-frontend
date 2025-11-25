e0072626836b39456689018d89a79798e16cf528
Pratush <pratushbarua26@gmail.com>
2025-11-25 09:56:23 +0600

    changed config file

--- vite.config.js
+++ vite.config.js
@@ -5,21 +5,5 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: './', // This fixes path issues on Firebase
-  build: {
-    outDir: 'dist',
-    emptyOutDir: true,
-    // Add this for better chunking
-    rollupOptions: {
-      output: {
-        manualChunks: {
-          vendor: ['react', 'react-dom'],
-          ui: ['react-router-dom']
-        }
-      }
-    }
-  },
-  server: {
-    port: 3000
-  }
+  base: './'
 })
